3c83847dedf48b664a7d95325e6c633df239b27d
rum <rumeshranaweera99@gmail.com>
2023-05-01 09:55:59 +0530

    add auth firebase and providor

---
+++ firebase.ts
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDWd4hEGIh6mN35EenGlzzE5rL9MZsZ38w",
+  authDomain: "fir-a47dc.firebaseapp.com",
+  projectId: "fir-a47dc",
+  storageBucket: "fir-a47dc.appspot.com",
+  messagingSenderId: "202725781255",
+  appId: "1:202725781255:web:30e1dc05d4e73180d51fa2",
+};
---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,14 @@
+import NextAuth, { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+export const authOptions: AuthOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+    // ...add more providers here
+  ],
+};
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
