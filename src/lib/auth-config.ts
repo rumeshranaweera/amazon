8c6e49d42485a36539609a96c1a0696cace2739e
rum <rumeshranaweera99@gmail.com>
2023-09-03 16:32:36 +0530

    change ts-config

---
+++ src/lib/auth-config.ts
@@ -0,0 +1,15 @@
+// auth-config.ts
+import { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const authOptions: AuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+    // Add more providers here if needed
+  ],
+};
+
+export default authOptions;
